0279f6f18f42a55727cb7e37a23dc2c31f6ba3cf
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2023-10-22 19:16:42 -0300

    Create Atividade01_Questão01.py

---
+++ Cálculo IV/Atividade01_Questão01.py
@@ -0,0 +1,76 @@
+#Atividade 1 - Questão 1
+#Lucas Silva Clemente
+
+#embora neste código nenhum método desta biblioteca esteja sendo usado diretamente
+#pode ser que o usuário queria usar na expressão de entrada. Ex: sqrt()
+import math
+
+#método que define a função do termo a[n] que será utilizada
+def termo(n, funcao):
+    try:
+        #avalia a expressão de entrada, quer será executada como uma instrução de Python
+        return eval(funcao) 
+
+    # Tratamento para divisão por zero
+    except ZeroDivisionError:
+        return float("inf")  
+
+#método que calcula o limite
+def limiteSeq(maxN):
+
+    funcao = input("Digite aqui o termo a[n]: ")
+
+    oldLim = termo(0, funcao)
+    newLim = oldLim
+
+    for n in range(1, maxN + 1):
+
+        termoSeq = termo(n, funcao)
+        
+        #caso o termo seja infinito, retorna "infinito"
+        if termoSeq == float("inf"):
+            return "Infinito"  
+        
+        #caso o termo seja None, retorna "inexistente"
+        if termoSeq is None:
+            return "Inexistente"
+        
+        newLim = termoSeq
+        
+        #comparamos aproximadamente o limite atual com o limite anterior
+        #se ambos os limites forem suficientemente próximos, retornamos o limite atual
+        #o número de casas decimais a serem aproximadas pode ser alterado ao seu critério
+        #quanto maior o numero de casas decimais a serem aproximadas, mais preciso o resultado é
+        if round(newLim, 8) == round(oldLim, 8):
+            return "O limite é: {:.2f}".format(newLim)
+    
+        oldLim = newLim
+
+    #se chegarmos ao limite de iterações, na realidade não podemos concluir se não existe ou é infinito
+    return "Fim da iteração, limite pode ser tanto infinito quanto inexistente" 
+
+def main(): 
+    
+    #define um limite máximo arbitrário, já que não podemos ter iterações infinitas
+    #assim como não podemos iterar até float("inf")
+    #é preferivel que seja escolhido um limite alto para maior confiabilidade dos resultados
+    #um limite muito baixo pode significar atingir o fim da iteração sem uma conclusão exata
+    #pode ser alterado ao seu critério
+    maxN = 1000000000
+
+    print(limiteSeq(maxN))
+
+if __name__ == "__main__":
+    main()
+
+#RESULTADOS DE ALGUNS TESTES
+#-------------------------------------------------------------------------------------------------------
+# ENTRADA                           |   RESULTADO
+#-------------------------------------------------------------------------------------------------------
+# None                              |   Inexistente
+# float("inf")                      |   Infinito
+# n                                 |   Indicou que atingiu o fim da iteração, mas deveria ser infinito
+# 1/(n**2)                          |   0.00
+# 1/(n + 1)                         |   0.00
+# (n + 1)/(3*n - 1)                 |   0.33
+# math.sqrt(n)/(1 + math.sqrt(n))   |   1.00       
